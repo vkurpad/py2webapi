60fa9ad50a74b0e0b27e1b642033d78947038437
vkurpad <vikurpad@microsoft.com>
2018-11-11 00:35:36 -0800

    no errors, but still not getting intended result

--- web/project/__init__.py
+++ web/project/__init__.py
@@ -1,20 +1,36 @@
 from flask import Flask
+import os
+from tatk.pipelines.entity_extraction.dictionary_based_entity_extractor import DictionaryBasedEntityExtractor
+from pathlib import Path
+from flask import request, jsonify
+import pandas as pd
+
+p = Path(__file__).resolve().parents[2]
+
 app = Flask(__name__)
-model_file_path = os.join(os.path.realpath(__file__), "outputs")
+model_file_path = os.path.join(p, "outputs", "sebra_model.zip")
 print('loading the output model from the file {}'.format(model_file_path))
 loaded_entity_extractor = DictionaryBasedEntityExtractor.load(model_file_path)
 
-
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World! - Prediction for Vinod'
 
 
 @app.route('/predict', methods=["POST"])
 def predict():
-    data = request.json
-    df = pd.DataFrame.from_dict(data, orient='columns')
-    loaded_entity_extractor.transform(df)
-    df.head()
-    return jsonify(df)
+    data = request.get_json()
+    print(data["values"][0]["data"])
+    for rec in data["values"]:
+        df = pd.DataFrame({'col':rec["data"]["text"]})
+        
+        df.columns = ["description"]
+        print (df)
+        loaded_entity_extractor.transform(df)
+        df.head()
+    
+    return jsonify(df.to_json())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
     
